db75344ee905ea9ba2fdf102331eb6e790352bdf
Eugenio Guarino <eugenio.guarino@students.plymouth.ac.uk>
2019-02-24 23:54:20 +0000

    fixed unit tests

--- MyApplication/app/src/test/java/com/example/myapplication/MainActivityTest.java
+++ MyApplication/app/src/test/java/com/example/myapplication/MainActivityTest.java
@@ -1,57 +1,9 @@
 package com.example.myapplication;
-
-import android.arch.lifecycle.LifecycleRegistry;
-import android.arch.lifecycle.ViewModelStore;
-import android.content.res.Resources;
-import android.os.Handler;
-import android.support.v4.app.FragmentController;
-import android.support.v4.app.SupportActivity;
-import android.support.v4.util.SimpleArrayMap;
-import android.support.v4.util.SparseArrayCompat;
-import android.support.v7.app.AppCompatDelegate;
-import android.widget.Spinner;
-
-import com.android.volley.RequestQueue;
-
-import org.json.JSONObject;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import static org.mockito.Mockito.*;
 
 public class MainActivityTest {
-    @Mock
-    Spinner spinner;
-    @Mock
-    JSONObject APIdata;
-    @Mock
-    RequestQueue queue;
-    @Mock
-    AppCompatDelegate mDelegate;
-    @Mock
-    Resources mResources;
-    @Mock
-    Handler mHandler;
-    @Mock
-    FragmentController mFragments;
-    @Mock
-    ViewModelStore mViewModelStore;
-    @Mock
-    SparseArrayCompat<String> mPendingFragmentActivityResults;
-    @Mock
-    SimpleArrayMap<Class<? extends SupportActivity.ExtraData>, SupportActivity.ExtraData> mExtraDataMap;
-    @Mock
-    LifecycleRegistry mLifecycleRegistry;
-    @InjectMocks
     MainActivity mainActivity;
-
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
 
     @Test
     public void testOnCreate() throws Exception {
